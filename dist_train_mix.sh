3be2af1574e7a8103055298463a933d750937d3e
shota74 <syouta74_vision_book@icloud.com>
2022-01-12 22:53:41 +0900

    a

--- dist_train_mix.sh
+++ dist_train_mix.sh
@@ -1,13 +1,13 @@
 
-python -m torch.distributed.launch --nproc_per_node=8 --master_port=29509 dist_train_mix.py --dataset Synscapes_to_cityscapes_100   --dataset_sorcee synscapes  --dataset_target cityscapes_100
+python -m torch.distributed.launch --nproc_per_node=8 --master_port=29508 dist_train_mix.py --dataset Synscapes_to_cityscapes_100   --dataset_sorcee synscapes  --dataset_target cityscapes_100
 
-python -m torch.distributed.launch --nproc_per_node=8 --master_port=29509 dist_train_mix.py --dataset Synscapes_to_cityscapes_200   --dataset_sorcee synscapes  --dataset_target cityscapes_200
+python -m torch.distributed.launch --nproc_per_node=8 --master_port=29508 dist_train_mix.py --dataset Synscapes_to_cityscapes_200   --dataset_sorcee synscapes  --dataset_target cityscapes_200
 
-python -m torch.distributed.launch --nproc_per_node=8 --master_port=29509 dist_train_mix.py --dataset Synscapes_to_cityscapes_500   --dataset_sorcee synscapes  --dataset_target cityscapes_500
+python -m torch.distributed.launch --nproc_per_node=8 --master_port=29508 dist_train_mix.py --dataset Synscapes_to_cityscapes_500   --dataset_sorcee synscapes  --dataset_target cityscapes_500
 
-python -m torch.distributed.launch --nproc_per_node=8 --master_port=29509 dist_train_mix.py --dataset Synscapes_to_cityscapes_1000   --dataset_sorcee synscapes  --dataset_target cityscapes_1000
+python -m torch.distributed.launch --nproc_per_node=8 --master_port=29508 dist_train_mix.py --dataset Synscapes_to_cityscapes_1000   --dataset_sorcee synscapes  --dataset_target cityscapes_1000
 
 
-python -m torch.distributed.launch --nproc_per_node=8 --master_port=29509 dist_train_mix.py --dataset GTA5_to_cityscapes_100   --dataset_sorcee GTA5  --dataset_target cityscapes_100
-python -m torch.distributed.launch --nproc_per_node=8 --master_port=29509 dist_train_mix.py --dataset GTA5_to_cityscapes_200   --dataset_sorcee GTA5  --dataset_target cityscapes_200
-python -m torch.distributed.launch --nproc_per_node=8 --master_port=29509 dist_train_mix.py --dataset GTA5_to_cityscapes_1000   --dataset_sorcee GTA5  --dataset_target cityscapes_1000
+python -m torch.distributed.launch --nproc_per_node=8 --master_port=29508 dist_train_mix.py --dataset GTA5_to_cityscapes_100   --dataset_sorcee GTA5  --dataset_target cityscapes_100
+python -m torch.distributed.launch --nproc_per_node=8 --master_port=29508 dist_train_mix.py --dataset GTA5_to_cityscapes_200   --dataset_sorcee GTA5  --dataset_target cityscapes_200
+python -m torch.distributed.launch --nproc_per_node=8 --master_port=29508 dist_train_mix.py --dataset GTA5_to_cityscapes_1000   --dataset_sorcee GTA5  --dataset_target cityscapes_1000
